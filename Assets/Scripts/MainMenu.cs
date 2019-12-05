1764cdf31023ed5202a11a5ac681c8d3863354cf
dom16 <35589924+dom16@users.noreply.github.com>
2019-12-05 00:37:08 +0000

    Hand Feature Started

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour
+{
+    //Variable to control the Audio Mixer
+    public AudioMixer audioMixer;
+
+    //This method will run when the play selects Play button
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    //This method will run when the Exit button is selected
+    public void QuitGame()
+    {
+        Debug.Log("Exit");
+        Application.Quit();
+    }
+
+    //This method will control the volume of the Game
+    public void SetVolume(float volume)
+    {
+        audioMixer.SetFloat("Volume", volume);
+    }
+
+    //This controls if the game is in fullscreen or not in fullscreen
+    public void SetFullscreen(bool isFullscreen)
+    {
+        Screen.fullScreen = isFullscreen;
+    }
+}
